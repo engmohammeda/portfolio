bfe9d74ba4dcbad203689cb1c026338c13cee97d
Mohammed Al-Bukhaiti <mohammedalbkhyty@gmail.com>
2025-07-12 12:45:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+// src/App.tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// -->> قم بتغيير السطر التالي <<--
+import { HashRouter, Routes, Route } from "react-router-dom"; // استبدل BrowserRouter بـ HashRouter
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,13 +16,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* -->> وقم بتغيير المكون هنا <<-- */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
